c8e2cd8dba08d67a245dd55b21f5879bc8ee2e4e
高成鑫 <43144265+gaochengxin123@users.noreply.github.com>
2020-04-20 01:58:08 +0800

    20.0420第一次提交 01:58

--- ImgPro/ImgProView.cpp
+++ ImgPro/ImgProView.cpp
@@ -31,6 +31,8 @@ BEGIN_MESSAGE_MAP(CImgProView, CView)
 	ON_COMMAND(ID_FILE_PRINT_DIRECT, CView::OnFilePrint)
 	ON_COMMAND(ID_FILE_PRINT_PREVIEW, CView::OnFilePrintPreview)
 	ON_COMMAND(ID_32771, &CImgProView::Color)
+	ON_COMMAND(ID_32772, &CImgProView::Imgsegmentation)
+	ON_COMMAND(ID_32773, &CImgProView::imgdivided)
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -83,7 +85,6 @@ void CImgProView::OnDraw(CDC* pDC)
 			{
 				gray = image[i*width+j];
 				pDC->SetPixel( j, i, RGB(gray,gray,gray));
-
 			}
 		}
 	}
@@ -91,6 +92,7 @@ void CImgProView::OnDraw(CDC* pDC)
 
 	if(bmpflag == 1)
 	{
+		pDC->TextOut(0, 0, "车牌识别：原始图像");
 		BYTE r,g,b;
 		for( i=0; i<height; i++ )
 			for( j=0; j<3*width; j=j+3 )
@@ -98,35 +100,116 @@ void CImgProView::OnDraw(CDC* pDC)
 				b = rgbimg[i*3*width+j];
 				g = rgbimg[i*3*width+j+1];
 				r = rgbimg[i*3*width+j+2];
-				pDC->SetPixelV( j/3, i, RGB(r, g, b) );
+				pDC->SetPixelV( j/3, i+30, RGB(r, g, b) );
 			}
 	}
 	if(flag == 1)
 	{
 		BYTE r, g, b;
 		BYTE gray;
-		if(huiimg)
+		if (0)				//设置为1可看出在图像处理过程中的各个步骤，但容易导致程序卡死
 		{
-			for( i=0; i<height; i++)
+			if (huiimg2)
 			{
-				for( j=0; j<width; j++)
+				pDC->TextOut(width + 10, 0, "进行颜色识别，判断出符合车牌颜色的元素");
+				for (i = 0; i < height; i++)
 				{
-					if (huiimg[i * width + j] == 0) {
-						gray = 255;
-						pDC->SetPixel(j + width + 10, i, RGB(gray, gray, gray));
-					}
-					else
+					for (j = 0; j < width; j++)
 					{
-						b = rgbimg[i * 3 * width + j*3];
-						g = rgbimg[i * 3 * width + j *3+ 1];
-						r = rgbimg[i * 3 * width + j *3+ 2];
-						pDC->SetPixel(j + width + 10,i,RGB(r, g, b));
+						gray = huiimg2[i * width + j];
+						pDC->SetPixel(j + width + 10, i + 30, RGB(gray, gray, gray));
 					}
-					
-
+				}
+			}
+			if (huiimg3)
+			{
+				pDC->TextOutA(width * 2 + 10 * 2, 0, "腐蚀与膨胀，准备判断车牌范围");
+				for (i = 0; i < height; i++)
+				{
+					for (j = 0; j < width; j++)
+					{
+						gray = huiimg3[i * width + j];
+						pDC->SetPixel(j + width * 2 + 10 * 2, i + 30, RGB(gray, gray, gray));
+					}
+				}
+			}
+			if (huiimg)
+			{
+				pDC->TextOutA(width * 3 + 10 * 3, 0, "判断车牌范围");
+				for (i = 0; i < height; i++)
+				{
+					for (j = 0; j < width; j++)
+					{
+						gray = huiimg[i * width + j];
+						pDC->SetPixel(j + width * 3 + 10 * 3, i + 30, RGB(gray, gray, gray));
+					}
 				}
 			}
 		}
+		else if (huiimg)
+		{
+			pDC->TextOutA(width + 10, 0, "颜色分割：判断车牌范围");
+			for (i = 0; i < height; i++)
+			{
+				for (j = 0; j < width; j++)
+				{
+					gray = huiimg[i * width + j];
+					//pDC->SetPixel(j + width * 3 + 10 * 3, i + 30, RGB(gray, gray, gray));
+					pDC->SetPixel(j + width + 10, i + 30, RGB(gray, gray, gray));
+				}
+			}
+		}
+	}
+
+	if (segflag == 1)
+	{
+		BYTE gray;
+		BYTE r, g, b;
+		if (flag == 0)
+			pDC->TextOutA(width + 10, 0, "车牌提取：分割出车牌");
+		else;
+		for (i = 0; i < newheight; i++)
+		{
+			for (j = 0; j < 3 * newwidth; j = j + 3)
+			{
+				b = newrgbimg[i * 3 * newwidth + j];
+				g = newrgbimg[i * 3 * newwidth + j + 1];
+				r = newrgbimg[i * 3 * newwidth + j + 2];
+				pDC->SetPixelV(j / 3 + width + 10, i + 30, RGB(r, g, b));
+			}
+		}
+/*		if (newhuiimg)
+		{
+			pDC->TextOutA(width + newwidth + 30, 10, "车牌图像灰度图片");
+			for (i = 0; i < newheight; i++)
+			{
+				for (j = 0; j < newwidth; j++)
+				{
+					gray = newhuiimg[i * newheight + j];
+					pDC->SetPixel(j + width + newwidth + 30, i + 30, RGB(gray, gray, gray));
+				}
+			}
+		}
+		else pDC->TextOutA(width + newwidth + 30, 10, "打开图片失败");
+*/
+	}
+	if (divflag == 1)
+	{
+		pDC->TextOutA(width + 10, 0, "扫描分割：");
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 66.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 66.5, 30 + newheight);
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 123.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 123.5, 30 + newheight);
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 145.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 145.5, 30 + newheight);
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 202.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 202.5, 30 + newheight);
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 259.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 259.5, 30 + newheight);
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 316.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 316.5, 30 + newheight);
+		pDC->MoveTo(width + 10 + newwidth / 440.0 * 373.5, 30);
+		pDC->LineTo(width + 10 + newwidth / 440.0 * 373.5, 30 + newheight);
 	}
 }
 
@@ -180,12 +263,16 @@ void CImgProView::OnFileOpen()
 	CFileDialog MyFDlg(TRUE, NULL,NULL, OFN_HIDEREADONLY, NULL,NULL );
 
 	MyFDlg.m_ofn.lpstrFilter = "(*.lst;*.raw;*.bmp)\0*.lst;*.raw;*.bmp\0";
-	MyFDlg.m_ofn.lpstrInitialDir = "C:\\Users\\娼樺厛閾\Desktop\\WPSLIB\\鍥惧儚澶勭悊澶т綔涓歕\3\\3\\鍥惧儚";
+	MyFDlg.m_ofn.lpstrInitialDir = "图像";
 
 	flag=0;
 	bmpflag = 0;
 	rawflag = 0;
-
+//*************************************************************
+//2019.12.21,刘皓郡，车牌分割和车牌扫描分割的标志位
+	segflag = 0;
+	divflag = 0;
+//*************************************************************
 	if( MyFDlg.DoModal() == IDOK )
 	{
 		CString SFName;
@@ -384,25 +471,33 @@ void CImgProView::OnFileSave()
 
 void CImgProView::Color()
 {
-	// TODO: 鍦ㄦ娣诲姞鍛戒护澶勭悊绋嬪簭浠ｇ爜
+	// TODO: 在此添加命令处理程序代码
     BYTE r,g,b;
 	int i,j;
 	double hg,hr;
 	double max1=0.0;
 	BYTE gray;
-	BYTE* huiimg1;
 	gray = 0;
-	
+	flag = 1;
+	OnInitialUpdate();
+
 	huiimg = new BYTE[width * height];
 	huiimg1 = new BYTE[width * height];
+	huiimg2 = new BYTE[width * height];
+	huiimg3 = new BYTE[width * height];
 	BYTE max, min, delta;
 	double h, s, v;
+	int helllworld;
 
-	for (int i = 0; i < height; i++)
+//******************************************
+//2019.12.19潘先铧：将RGB转化到HSV
+//******************************************
+
+	for (int i = 0; i < height; i++)																
 	{
 		for (int j = 0; j < width; j++)
 		{
-			b = rgbimg[i * 3 * width + j * 3];//i*3骞插槢
+			b = rgbimg[i * 3 * width + j * 3];
 			g = rgbimg[i * 3 * width + j * 3 + 1];
 			r = rgbimg[i * 3 * width + j * 3 + 2];
 
@@ -426,7 +521,10 @@ void CImgProView::Color()
 				if (max == b)
 					h = double(60 * (r - g)) / delta + 240;
 			}
-
+//******************************************
+//2019.12.19潘先铧：在HSV下利用图像颜色特征分割车牌
+//得到的是车牌区域图像的二值化图像，车牌区域白色，其他黑色
+//******************************************
 			v = max;
 			if (v == 0)
 			{
@@ -436,7 +534,7 @@ void CImgProView::Color()
 				s = double(delta) / max;
 			if (h < 0)
 				h += 360;
-			if ((h <= 270.0 && 208.0 <= h) && s > 0.52)
+			if ((h <= 270.0 && 208.0 <= h) && s > 0.52)				//参数可根据实际情况调整，相较于下面的上面个这组参数效果较好
 			{
 				huiimg1[i * width + j] = 255;
 			}
@@ -444,29 +542,63 @@ void CImgProView::Color()
 			{
 				huiimg1[i * width + j] = 0;
 			}
-			if ((h <= 270.0 && 205.0 <= h) && s > 0.48)
-			{
-				huiimg[i * width + j] = 255;
-			}
-			else
-			{
-				huiimg[i * width + j] = 0;
-			}
+			//if ((h <= 270.0 && 205.0 <= h) && s > 0.48)
+			//{
+				//huiimg[i * width + j] = 255;
+			//}
+			//else
+			//{
+				//huiimg[i * width + j] = 0;
+			//}
+			huiimg2[i * width + j] = huiimg1[i * width + j];
 		}
 
 	}
+
+//******************************************
+//2019.12.1沈灿红：对分割出来的图像进行腐蚀膨胀
+//******************************************
 	BYTE* tempout;
 	BYTE* tempout2;
 	tempout = new BYTE[width * height];
 	tempout2 = new BYTE[width * height];
 	
-	erosion(huiimg1, width, height, tempout,1);         //first鑵愯殌then鑶ㄨ儉
+	erosion(huiimg1, width, height, tempout,1);         //first腐蚀then膨胀
 	dilation(tempout, width, height, tempout2,2);
 	erosion(tempout2, width, height, tempout, 1);
 	dilation(tempout, width, height, huiimg1, 1);
+	
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			huiimg3[i * width + j] = huiimg1[i * width + j];
+		}
+	}
+//**************************************************************
+//2019.12.20潘先铧，根据腐蚀膨胀图像判断车牌范围
+//腐蚀膨胀中可能对大块污点处理不充分，
+//根据污点长度和车牌长度比较将其直接从图像中去除。
+//maxh,minh纵向范围，maxw,minw横向范围
+//**************************************************************									
+	maxw = height / 10; 
+	maxh = width / 10; 
+	minh = 9 * height / 10; 
+	minw = 9 * width / 10;
 
-	int maxh, maxw, minh, minw;
-	maxw = height / 10; maxh = width / 10; minh = 9 * height / 10; minw = 9 * width / 10;
+	for (i = 0; i < height; i++) {
+		int c = 0;
+		for (j = 0; j < width; j++) {
+			if (huiimg1[i * width + j] == 255)
+				c = c + 1;                                           
+		}
+		if (c < width / 20) {
+			for (j = 0; j < width; j++) {
+				huiimg1[i * width + j] =0;
+			}
+		}
+	}                                                          //去除大块污点
+
 
 	for (i = 0; i < height; i++) {
 		for (j = 0; j < width; j++) {
@@ -479,7 +611,7 @@ void CImgProView::Color()
 				}
 			}
 		}
-	}
+	}                                                                      //find    minw,maxw
 
 	for (i = 0; i < height; i++) {
 		for (j = 0; j < width; j++) {
@@ -492,20 +624,23 @@ void CImgProView::Color()
 				}
 			}
 		}
-	}
+	}                                                                      //find    minh,maxh
 
-	for (i = 0; i < height; i++) {
+	for (i = 0; i < height; i++) {											//根据找到的车牌范围处理图像
 		for (j = 0; j < width; j++) {
 			if (i >= minh && i <= maxh && j <= maxw && j >= minw) {
-				huiimg[i * width + j] = 255;
+				huiimg1[i * width + j] = 255;
 			}
 			else {
-				huiimg[i * width + j] = 0;
+				huiimg1[i * width + j] = 0;
 			}
+			huiimg[i * width + j] = huiimg1[i * width + j];
 		}
 	}
-		flag=1;
-		OnInitialUpdate();
+//2019.12.21刘皓郡，定义新的保存分割出来的车牌的数组
+	newwidth = maxw - minw;
+	newheight = maxh - minh;
+	newrgbimg = new BYTE[3 * newwidth * newheight];
 }
 
 void CImgProView::dilation(BYTE* image, int w, int h, BYTE* outImg, int p)
@@ -562,4 +697,69 @@ void CImgProView::erosion(BYTE* image, int w, int h, BYTE* outImg,int p)
 		}
 		memcpy(image, outImg, sizeof(BYTE) * width * height);
 	}
+}
+
+//*************************************************************
+//2019.12.21高成鑫，车牌区域图像的提取
+//*************************************************************
+
+void CImgProView::Imgsegmentation()
+{
+	// TODO: 在此添加命令处理程序代码
+	flag = 0;
+	segflag = 1;
+	int i, j;
+	BYTE r, g, b;
+	BYTE gray;
+	newhuiimg = new BYTE[newwidth * newheight];
+	OnInitialUpdate();
+	if (huiimg)
+	{
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				if (huiimg[i * width + j] != 0) 
+				{
+					b= rgbimg[i * 3 * width + j * 3];
+					g= rgbimg[i * 3 * width + j * 3 + 1];
+					r= rgbimg[i * 3 * width + j * 3 + 2];
+					
+					newrgbimg[(i - minh) * newwidth * 3 + (j - minw) * 3] = b;
+					newrgbimg[(i - minh) * newwidth * 3 + (j - minw) * 3 + 1] = g;
+					newrgbimg[(i - minh) * newwidth * 3 + (j - minw) * 3 + 2] = r;
+
+					//newhuiimg[(i - minh) * newwidth + (j - minw)] = 0.110 * b + 0.588 * g + 0.302 * r;
+				}
+			}
+		}
+/*		for (i = 0; i < newheight; i++)
+		{
+			for (j = 0; j < newwidth; j++)
+			{
+				b = newrgbimg[i * newwidth * 3 + j * 3];
+				g = newrgbimg[i * newwidth * 3 + j * 3 + 1];
+				r = newrgbimg[i * newwidth * 3 + j * 3 + 2];
+				newhuiimg[i*newwidth+j]=BYTE(0.110 * b + 0.588 * g + 0.302 * r);
+			}
+		}
+*/
+	}
+}
+//*************************************************************
+//2019.12.22，高成鑫，车牌图像的扫描分割
+//按比例分割图片
+//容易受车牌倾斜、图片角度变化影响
+//应该对图像进行处理，如霍夫变换纠正倾斜图像
+//同理，分割时更好的处理方法应该是
+//基于灰度图像对图像进行连通域分析
+//*************************************************************
+
+void CImgProView::imgdivided()
+{
+	// TODO: 在此添加命令处理程序代码
+	divflag = 1;
+	flag = 2;
+	segflag = 1;
+	OnInitialUpdate();
 }
